29d4fd72d72f430a871b2174c268b4d8996274d1
Harish-Chandar <harishschandar@gmail.com>
2023-11-04 19:12:19 -0500

    Testing paras

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,20 @@
 <script>
     let count = 0;
+    let paracontent = "see look it changed";
     function increment(){
         count ++;
+        if (paracontent === "see look it changed"){
+            paracontent = "it changed again wow";
+        } else {
+            paracontent = "see look it changed"
+        }
     }
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <button on:click={increment}> {count} </button>
+<p id="para"> {paracontent} </p>
 
 <style>
     
